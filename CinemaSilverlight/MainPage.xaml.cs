48549e1306702bcd4ece0e7b873473d868402ee2
Renato Freire Ricardo <renatorfr@gmail.com>
2012-03-13 14:57:29 -0300

    Initial Commit

---
+++ CinemaSilverlight/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using CinemaSL.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+
+namespace CinemaSL
+{
+    public partial class MainPage : UserControl
+    {
+        public ObservableCollection<Filme> Filmes = new ObservableCollection<Filme>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIncluirFilme_Click(object sender, RoutedEventArgs e)
+        {
+            Filmes.Add(new Filme(txtFilme.Text, Convert.ToInt32(txtDuracao.Text)));
+
+            cmbFilmes.DataContext = Filmes;
+        }
+
+        private void btnIncluirSecao_Click(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<Filme> filmesAntes = new ObservableCollection<Filme>(Filmes);
+
+            int cmbSelectedIndex = cmbFilmes.SelectedIndex;
+
+            foreach (Filme filme in filmesAntes)
+            {
+                if (filme.Nome.Equals((cmbFilmes.SelectedValue as Filme).Nome))
+                {
+                    int filmeIndex = filmesAntes.IndexOf(filme);
+
+                    filme.AdicionarSecao(Convert.ToDateTime(txtSecao.Text));
+                    Filmes[filmeIndex] = filme;
+                }
+            }
+
+            cmbFilmes.SelectedIndex = cmbSelectedIndex;
+
+            txtSecao.Text = string.Empty;
+            txtSecao.Focus();
+        }
+
+        private void btnGerarTabela_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CinemaSilverlight/Models/Filme.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CinemaSL.Models
+{
+    public class Filme
+    {
+        public Filme() { }
+
+        public Filme(string nome, int duracao)
+        {
+            this.Nome = nome;
+            this.Duracao = duracao;
+        }
+
+        public string Nome { get; set; }
+        public int Duracao { get; set; }
+        public List<string> Secoes = new List<string>();
+
+        public override string ToString()
+        {
+            return Nome;
+        }
+
+        public void AdicionarSecao(DateTime horario)
+        {
+            Secoes.Add(horario.ToString());
+        }
+    }
+}
